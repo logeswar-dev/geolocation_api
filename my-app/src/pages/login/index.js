eef57c0197ee1adae731a2b07b95886d671e07bb
Logeswar Madhavan <loki.inreallife@gmail.com>
2022-05-24 09:14:34 -0400

    intitiating api

---
+++ my-app/src/pages/login/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div className="main-app">
+                <h1>testing</h1>
+            </div>
+        );
+    }
+};
+
+export default Login;
